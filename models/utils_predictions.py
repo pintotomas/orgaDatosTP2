41765345eaf0827656405ac9673cd61b2460388a
pintotomas <tomasp834@gmail.com>
2017-11-21 18:02:23 -0300

    fix

--- models/utils_predictions.py
+++ models/utils_predictions.py
@@ -6,13 +6,18 @@ def make_predictions(model, test_set, final_features, file_name):
 	"""model: el modelo entrenado
 	   test_set: registros con los features que ayudan a predecir
            final_features: features usados para entrenar el modelo, si se usaron todos es test_set.columns
-           file_name: nombre del archivo donde guardar los resultados"""
+           file_name: nombre del archivo donde guardar los resultados
+	   
+	   escribe un archivo con (id,price_usd) con las predicciones del modelo y lo guarda con el nombre
+	   file_name"""
 
 	i = 0
-	with open('knn_predictions2.csv', 'wb') as csvfile:
+	ids = list(test_set["id"].values)
+	predictions = model.predict(test_set[final_features])
+	with open(file_name, 'wb') as csvfile:
     		scorewriter = csv.writer(csvfile, delimiter=',',
                             quotechar=',', quoting=csv.QUOTE_MINIMAL)
-		scorewriter.writerow("id","price_usd")
+		scorewriter.writerow("id,price_usd")
     		while i < len(ids):
-        		scorewriter.writerow([ids[i],knn_predictions[i]])
+        		scorewriter.writerow([ids[i],predictions[i]])
         		i+=1
